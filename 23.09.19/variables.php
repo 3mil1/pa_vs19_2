941d0e265990495ab94ea8d3dacd8375521e8c69
Emil Varnomasig <emilvarnomasig@MacBook-Pro-Emil.local>
2019-09-23 12:26:20 +0300

    muutuhjate pohiandmete kasutus

---
+++ 23.09.19/variables.php
@@ -0,0 +1,17 @@
+<?php
+// muutujate defineerimine + vaartuse omistamine
+// $muutujaNimi = vaartus;
+//
+// andmetuubid
+//soned - strings
+// 'teksti kujulided andemd'
+// numbrid -taisarvud - 1,2,175, -25 jne;
+// numbrid - reaalarvud - 1.5, -2.98,
+$nimi = 'Emil Varn';
+$yksus = 'ikt osakond';
+$email = 'emilvarn@gmail.com';
+
+//muutujate kasutamine - kutsumine
+echo $nimi . '<br>';
+echo $yksus . '<br>';
+echo $email;
