51a0a9a61e2bf49842e514b04b8ed098d38a3cec
Emil Varnomasig <emilvarnomasig@MacBook-Pro-Emil.local>
2019-09-23 11:39:19 +0300

    Echo

---
+++ 23.09.19/Print.php
@@ -0,0 +1,8 @@
+<?php
+//printimine print funktsiooni abil
+print 'see on esimene php rida<br>';
+print 'see on teine php rida<br>';
+print 'see on kolmas php rida<br>';
+
+// echo abil printimine
+echo 'see on tekst echo abil';
